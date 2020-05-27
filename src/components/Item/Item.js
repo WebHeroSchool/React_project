6d9fa4e1f933ea4df2296a6245d5a6a251c23493
Alina Omelchenko <omelchenkoalina96@gmail.com>
2020-05-27 15:47:51 +0200

    -

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,6 +1,8 @@
+  
 import React from 'react';
 import classname from 'classnames';
 import styles from './Item.module.css';
+import PropTypes from 'prop-types';
 
 const Item = ({value, isDone, index, onClickDone, id}) => (
     <label
@@ -13,5 +15,15 @@ const Item = ({value, isDone, index, onClickDone, id}) => (
             })}>
         {value}
     </label>);
+
+Item.defaultProps = {
+    value: 'Тут должно быть задание',
+    isDone: false
+};
+
+Item.propTypes = {
+    onClickDone: PropTypes.func.isRequired,
+    value: PropTypes.string.isRequired
+};
 
 export default Item;
